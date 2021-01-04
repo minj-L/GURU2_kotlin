917b0ece99df402df1a48b46474f38441689fc28
minj-L <52521457+minj-L@users.noreply.github.com>
2021-01-04 18:35:15 +0900

    코틀린 변수

---
+++ exam01.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var var1  = 10
+    var var2  = 10.1f
+    var var3  = 10.2
+    var var4  = 'A'
+    var var5  = "Android"
+
+    println(var1)
+    println(var2)
+    println(var3)
+    println(var4)
+    println(var5)
+}
