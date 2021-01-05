de13bd6a0626a96856f17b23cc8f3fb9c112d20e
minj-L <52521457+minj-L@users.noreply.github.com>
2021-01-05 23:34:03 +0900

    for 반복문

---
+++ ForSum.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var sum = 0
+
+    for (x in 1..10) {
+        sum += x
+    }
+    println("sum: $sum")
+
+    sum = 0
+
+    for (x in 1..10 step 1) sum += x
+    println("sum: $sum")
+}
